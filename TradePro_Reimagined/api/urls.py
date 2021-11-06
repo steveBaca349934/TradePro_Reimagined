f6c3f0862e53e97527b8dc23d56534fe21109772
steveBaca <smbaca99@gmail.com>
2021-11-06 14:42:40 -0400

    push

--- TradePro_Reimagined/api/urls.py
+++ TradePro_Reimagined/api/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from .views import RoomView
 
 urlpatterns = [
-    path('admin/',admin.site.urls),
-    path('api',include('api.urls')),
-    path('', include('frontend.urls'))
+    #path('admin/',admin.site.urls),
+    #path('api',include('api.urls')),
+    path('room/', include('frontend.urls'))
+    #path('/room/', RoomView)
+    
 
 ]
