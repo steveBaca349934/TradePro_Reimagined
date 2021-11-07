d1ea8ecae2074a6f3a444cf4f368b5f9a730bbbf
steveBaca <smbaca99@gmail.com>
2021-11-07 17:00:24 -0500

    push

--- TradePro_Reimagined/frontend/src/components/HomePage.js
+++ TradePro_Reimagined/frontend/src/components/HomePage.js
@@ -1,23 +1,44 @@
-import React, {Component} from "react";
-import RoomJoinPage from "./RoomJoinPage";
-import CreateRoomJoinPage from "./CreateRoomJoinPage";
-import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
+import React, { Component } from "react";
+import CreateRAT from "./CreateRAT";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 
+class ImplementHomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-export default class HomePage extends Component{
-    constructor(props){
-        super(props);
-    }
+  render() {
 
-    render(){
-        return (
-        <Router>     
-            <Switch>
-                <Route exact path = '/'><p>This is the homepage</p></Route>
-                <Route path="/join" component={RoomJoinPage} />
-                <Route path = '/create' component = {CreateRoomJoinPage} /> 
-            </Switch>
-        </Router>);
-    }
+    return (
+
+      <p>We are implementing the home page</p>
+      
+    );
+
+
+  }
+}
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ImplementHomePage} />
+          <Route path="/RAT" component={CreateRAT} />
+        </Switch>
+      </Router>
+    );
+  }
 }
--- TradePro_Reimagined/frontend/src/components/app.js
+++ TradePro_Reimagined/frontend/src/components/app.js
@@ -1,42 +1,21 @@
+
 import React, { Component } from "react";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
 
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-
-/**
- * Note -> we need "export" in order to export a class to another file...
- * Similar to __init__.py in python
- */
-
-export default class App extends Component{
-
-    constructor(props){
-        /*
-        a prop is an 'element' of sorts that we pass
-        to the component, that the component can then utilize
-        */
-        super(props);
-    }
-
-    render(){
-
-        /**
-         * whenever we utilize the square brackets {}
-         * this allows us to insert javascript into our html text
-         */
-        return (<div>
-            <HomePage />
-     
-            </div>
-            );
-    }
-
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
-
 const appDiv = document.getElementById("app");
-/*this takes the "render()" function we declared above 
-and inserts this into the appDiv from index.html
-*/
-render(<App name="Steve" />, appDiv);
+render(<App />, appDiv);
