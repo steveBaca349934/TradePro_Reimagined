502e2c30802b7ff6f4bf13be84dbaab03a9186fc
steveBaca <smbaca99@gmail.com>
2021-11-08 17:36:59 -0500

    finally making some progress

--- TradePro_Reimagined/frontend/src/components/HomePage.js
+++ TradePro_Reimagined/frontend/src/components/HomePage.js
@@ -7,6 +7,15 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
 
 class ImplementHomePage extends Component {
@@ -14,12 +23,22 @@ class ImplementHomePage extends Component {
     super(props);
   }
 
+  /**
+   * Notes for myself about this render() function
+   * <h1> div = the first header tag visible on a page
+   * 
+   * 
+   */
   render() {
 
     return (
 
-      <p>We are implementing the home page</p>
-      
+      <div class="intro">
+        <h1 class ="logo-header">
+          <span class ="logo">TradePro Reimagined</span>
+        </h1>
+      </div>
+
     );
 
 
