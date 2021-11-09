df38a7d48d1ceb10c79efe3cd9ba38bb883f7df6
steveBaca <smbaca99@gmail.com>
2021-11-09 18:37:28 -0500

    push

--- TradePro_Reimagined/frontend/src/components/HomePage.js
+++ TradePro_Reimagined/frontend/src/components/HomePage.js
@@ -17,64 +17,12 @@ import Radio from "@material-ui/core/Radio";
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 
-/**
- * so these are the classes we 
- * are returning from the ImplementHomePage Render() function
- * kinda stupid that Im mixing OOP and Functional Programming
- * Will clean up later
- */
- let intro = document.querySelector(".intro")
- let lego = document.querySelector(".logo-header")
- let logoSpan = document.querySelector(".logo")
- 
-
-/**
- * good ole lambda function which triggers on webpage loading
- */
-window.addEventListener("DOMContentLoaded", () => {
-
-  setTimeout(() => {
-    logoSpan.forEach((span, idx) => {
-      setTimeout(() => {
-
-        span.classList.add("active");
-
-      }, (idx + 1) * 400)
-
-    });
-
-    setTimeout(() => {
-      logoSpan.forEach((span, idx) => {
-
-        setTimeout(() => {
-
-
-          span.classList.remove("active");
-          span.classList.add("fade");
-
-
-        }, (idx + 1) * 50)
-
-      })
-
-    }, 2000);
-
-    setTimeout(() => {
-      intro.style.top = '-100vh';
-
-
-    }, 2300)
-
-  })
-
-})
-
-
 
 class ImplementHomePage extends Component {
   constructor(props) {
     super(props);
   }
+  
 
   /**
    * Notes for myself about this render() function
@@ -84,11 +32,16 @@ class ImplementHomePage extends Component {
    */
   render() {
 
+   
+
     return (
+
+      
 
       <div class="intro">
         <h1 class ="logo-header">
           <span class ="logo">TradePro Reimagined</span>
+          <button id ="intro-page-button">Sample Button</button>
         </h1>
       </div>
 
