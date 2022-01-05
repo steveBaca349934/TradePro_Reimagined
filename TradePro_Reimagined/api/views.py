e08c0f07a33c65a3bf1bdd0ddae1333c2c33f42b
steveBaca <smbaca99@gmail.com>
2022-01-05 08:19:41 -0500

    working on login functionality

--- TradePro_Reimagined/api/views.py
+++ TradePro_Reimagined/api/views.py
@@ -25,7 +25,14 @@ def log_in(request):
 
     })
 
-def add_email_and_pw(request):
+def add_email_and_pw_login(request):
+    """
+    provides functionality to allow a user to login
+
+    returns the login page with either success or failure message
+    """
+    # TODO: Actually take email and password and check vs models.py that this is an actual user
+    # Also need to percolate and persist across this users entire session
 
     if request.method == 'POST':
 
@@ -40,8 +47,57 @@ def add_email_and_pw(request):
 
 
 def open_an_account(request):
+    """
+    Basic rendering of open_an_account page
 
-    return render(request, "home/open_an_account.html")
+    returns render
+    """
+
+
+    return render(request, "home/open_an_account.html",{
+        "newacc_form": login_form
+
+
+
+    })
+
+def add_email_and_pw_newacc(request):
+    """
+    Provides functionality that allows a user
+    to actually create an account... adds email and pw
+    to database
+
+    returns render
+    """
+
+    if request.method == 'POST':
+
+        print(request.POST)
+
+        # First step, ensure that email and 
+        # password were both entered        
+        if (len(request.POST['email']) == 0):
+            return render(request, "home/open_an_account.html",{
+            "newacc_form": login_form,
+            "email_provided": False
+
+    })
+
+        if len(request.POST['password']) == 0:
+
+            return render(request, "home/open_an_account.html",{
+            "newacc_form": login_form,
+            "pw_provided": False
+
+    })
+            
+
+        return render(request, "home/open_an_account.html",{
+        "newacc_form": login_form
+
+
+
+    })
 
 def profile(request):
 
