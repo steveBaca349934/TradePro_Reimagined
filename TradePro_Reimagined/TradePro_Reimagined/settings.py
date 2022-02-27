cc9d41539dbf11310cc63e2ca3b7a81ed24c5c4d
steveBaca <smbaca99@gmail.com>
2022-02-27 09:54:01 -0500

    preparing for production

--- TradePro_Reimagined/TradePro_Reimagined/settings.py
+++ TradePro_Reimagined/TradePro_Reimagined/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)xq(!(do15h2tyw$a7y2kexu)6h)ncyim_&3*=m+)4#+y+ovch'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = str(os.environ.get('DEBUG')) == '1'
+
+
 
 ALLOWED_HOSTS = []
 
@@ -80,6 +83,35 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DB_USERNAME = os.environ.get("POSTGRES_USER")
+DB_PASSWORD = os.environ.get("POSTGRES_DB")
+DB_DATABASE = os.environ.get("POSTGRES_DB")
+DB_HOST = os.environ.get("POSTGRES_HOST")
+DB_PORT = os.environ.get("POSTGRES_PORT")
+
+DB_IS_AVAIL = all([
+DB_USERNAME,
+DB_PASSWORD,
+DB_DATABASE,
+DB_HOST,
+DB_PORT
+])
+
+POSTGRES_READY=str(os.environ.get('POSTGRES_READY')) == '1'
+
+if DB_IS_AVAIL and POSTGRES_READY:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": DB_DATABASE,
+            "USER": DB_USERNAME,
+            "PASSWORD": DB_PASSWORD,
+            "HOST": DB_HOST,
+            "PORT": DB_PORT,
+        }
+    }
+
 
 
 # Password validation
